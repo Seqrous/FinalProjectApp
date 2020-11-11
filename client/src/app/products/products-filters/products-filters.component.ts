dd318f49c1e50458656003191436340ca91c7761
Krzysiek <krzysiek.ziach@gmail.com>
2020-11-11 17:54:59 +0100

    FP-38 Added 'products-filters' components

---
+++ client/src/app/products/products-filters/products-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fuse-products-filters',
+  templateUrl: './products-filters.component.html',
+  styleUrls: ['./products-filters.component.scss']
+})
+export class ProductsFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
