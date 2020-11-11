94dda6a4ee229c00bc6a96d0210585fb30c690c9
Krzysiek <krzysiek.ziach@gmail.com>
2020-11-11 13:40:38 +0100

    FP-11/FP-32 - toolbar now redirects to 'products/' and includes name parameter as well

--- client/src/app/products/product.service.ts
+++ client/src/app/products/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Product } from './models/product';
 import { map } from 'rxjs/internal/operators/map';
 import { mapToHttpParams } from 'app/common/utils/map-to-http-params';
+import { ProductQuery } from './models/product-query';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +12,7 @@ export class ProductService {
 
 constructor(private http: HttpClient) { }
 
-  getProducts(query: any): Observable<Product[]> {
+  getProducts(query: ProductQuery): Observable<Product[]> {
     const httpParams = mapToHttpParams(query);
     return this.http.get<Product[]>(`/products`, { params: httpParams }).pipe(map(data => data as Product[]));
   }
