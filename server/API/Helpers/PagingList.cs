ea76f3f956939e22ef931d2371dcd75bd1571724
svet0414 <46649699+svet0414@users.noreply.github.com>
2020-11-13 15:32:56 +0100

    Pagination works

--- server/API/Helpers/PagingList.cs
+++ server/API/Helpers/PagingList.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using server.API.DTOs.Products;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,55 +11,25 @@ namespace API.Entities
    
     public class PagingList<T> : List<T>
     {
-        //public List<T> Items = new List<T>();
-        private List<T> items = new List<T>();
-        //private var items = new List<T>();
-
-        public  List<T> Items
-        {
-            get
-            {
-                return items;
-            }
-
-            set { items = value; }
-        }
+        public List<T> Items { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
-        
-
-        private bool HasPrevious
-        {
-            get
-            {
-                return (CurrentPage > 1);
-            }
-        
-        }
-        private bool HasNext
-        {
-            get
-            {
-                return (CurrentPage < TotalPages);
-            }
-        }
+       
         public PagingList(List<T> items, int totalCount, int currentPage, int pageSize)
         {
             TotalCount = totalCount;
             PageSize = pageSize;
             CurrentPage = currentPage;
             TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
-            Items= items;
+            Items = items;
         }
         public static async Task<PagingList<T>> CreateList(IQueryable<T> source, int pageNumber, int pageSize)
         {
-            var total_count = await source.CountAsync();
+            var totalCount = await source.CountAsync();
             var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
-            return new PagingList<T>(items, total_count, pageNumber, pageSize);
-
-
+            return new PagingList<T>(items, totalCount, pageNumber, pageSize);
         }
     }
 }  
