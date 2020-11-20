9da03a62f0785a7db8a6b311f565d21f54254812
Krzysiek <krzysiek.ziach@gmail.com>
2020-11-20 13:54:23 +0100

    FP-40 Added 'ProductDetails' class and app now loads a product for the view

--- client/src/app/products/product/product.component.ts
+++ client/src/app/products/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { ProductDetails } from '../models/product-details';
+import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -9,6 +12,7 @@ import { Location } from '@angular/common';
 export class ProductComponent implements OnInit {
 
   product: any = {};
+  productId: number;
 
   /**
    * Constructor
@@ -16,17 +20,48 @@ export class ProductComponent implements OnInit {
    * @param _location 
    */
   constructor(
+    private _productService: ProductService,
+    private _route: ActivatedRoute,
     private _location: Location,
   ) { }
+
+  
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
 
   /**
    * On init
    */
   ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      this.productId = params['id'];
+    });
+
+    this.loadProduct(this.productId);
   }
+
+  
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
 
   back(): void {
     this._location.back();
   }
 
+  
+  // -----------------------------------------------------------------------------------------------------
+  // @ Private methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * 
+   * @param id 
+   */
+  private loadProduct(id: number): void {
+    this._productService.getProduct(id).subscribe(res => {
+      this.product = res;
+    });
+  }
 }
