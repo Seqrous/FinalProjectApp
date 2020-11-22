c41230b2245ebc62bea681906a794da5f3722e1f
Krzysiek <krzysiek.ziach@gmail.com>
2020-11-22 12:49:51 +0100

    Very basic content layout in quick panel - buttons, product information

--- client/src/app/layout/components/quick-panel/quick-panel.component.ts
+++ client/src/app/layout/components/quick-panel/quick-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ShoppingCartService } from 'app/common/services/shopping-cart.service';
+import { Product } from 'app/products/models/product';
 
 @Component({
     selector     : 'quick-panel',
@@ -6,17 +8,23 @@ import { Component, ViewEncapsulation } from '@angular/core';
     styleUrls    : ['./quick-panel.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class QuickPanelComponent
+export class QuickPanelComponent implements OnInit
 {
     date: Date;
     events: any[];
     notes: any[];
     settings: any;
+    products: Product[];
+    selected: '1';
 
     /**
      * Constructor
+     * 
+     * @param _shoppingCart 
      */
-    constructor()
+    constructor(
+        private _shoppingCart: ShoppingCartService,
+    )
     {
         // Set the defaults
         this.date = new Date();
@@ -25,5 +33,29 @@ export class QuickPanelComponent
             cloud : false,
             retro : true
         };
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        this.getProducts();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Private methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Return a list of all products in the shopping cart
+     */
+    private  getProducts(): void {
+        this._shoppingCart.cartProducts$.subscribe(products => {
+            this.products = products;
+        });
     }
 }
