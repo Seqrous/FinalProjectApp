5685fb4513369eb15d5f827dd9b0e7a2b28a7134
Krzysiek <krzysiek.ziach@gmail.com>
2020-11-23 23:34:57 +0100

    FP-85 Added routing to 'user/checkout' and 'user-checkout' compnents

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserCheckoutComponent } from './user-checkout/user-checkout.component';
 import { UserComponent } from './user.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
                 path: '',
                 component: UserComponent,
             },
+            {
+                path: 'checkout',
+                component: UserCheckoutComponent,
+            }
         ]
     }
 ];
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user-routing.module';
+import { UserCheckoutComponent } from './user-checkout/user-checkout.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,7 @@ import { UserRoutingModule } from './user-routing.module';
   ],
   declarations: [
     UserComponent,
+    UserCheckoutComponent,
   ]
 })
 export class UserModule { }
