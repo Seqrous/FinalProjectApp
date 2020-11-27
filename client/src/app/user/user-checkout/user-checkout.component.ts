d519ad8d7812f1347ef30f1e8f240f909c635482
Krzysiek <krzysiek.ziach@gmail.com>
2020-11-27 11:49:24 +0100

    Added a few validators to the forms, stepper is now being disabled if form is invalid

--- client/src/app/user/user-checkout/user-checkout.component.ts
+++ client/src/app/user/user-checkout/user-checkout.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatVerticalStepper } from '@angular/material/stepper';
 import { ShoppingCartService } from 'app/common/services/shopping-cart.service';
 import { Product } from 'app/products/models/product';
 
@@ -11,22 +12,56 @@ import { Product } from 'app/products/models/product';
 })
 export class UserCheckoutComponent implements OnInit {
 
-  isLinear = false;
+  isLinear = true;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   products: Product[];
   checked = true;
 
+  @ViewChild('stepper') stepper: MatVerticalStepper;
+
+  /**
+   * Constructor
+   * 
+   * @param _formBuilder 
+   * @param _shoppingCartService 
+   */
   constructor(
     private _formBuilder: FormBuilder,
     private _shoppingCartService: ShoppingCartService,
     ) {}
 
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On init
+   */
   ngOnInit(): void {
     this.buildForms();
     this.loadProducts();
   }
 
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * Verify form step
+   * Toggle true if all fields have been filled correctly
+   */
+  public verifyStep(): void {
+    this.stepper.selected.completed = this.firstFormGroup.valid;
+  }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Private methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * Subscribe to the shopping cart's products
+   */
   private loadProducts(): void {
     this._shoppingCartService.cartProducts$.subscribe((products: Product[]) => {
       this.products = products;
@@ -37,21 +72,21 @@ export class UserCheckoutComponent implements OnInit {
 
   private buildForms(): void {
     this.firstFormGroup = this._formBuilder.group({
-      firstName: [''],
-      lastName: [''],
-      email: [''],
+      firstName: ['', [Validators.pattern('[a-zA-Z]*'), Validators.required]],
+      lastName: ['', [Validators.pattern('[a-zA-Z]*'), Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
       company: [''],
-      city: [''],
-      zip: [''],
-      streetAddress: [''],
-      country: [''],
+      city: ['', Validators.required],
+      zip: ['', [Validators.pattern('^[a-z0-9][a-z0-9\- ]{0,10}[a-z0-9]$'), Validators.required]],
+      streetAddress: ['', Validators.required],
+      country: ['', [Validators.pattern('[a-zA-Z]*'), Validators.required]],
     });
     this.secondFormGroup = this._formBuilder.group({
-      name: [''],
-      creditCardNumber: [''],
-      verificationNumber: [''],
-      expirationMonth: [''],
-      expirationYear: ['']
+      name: ['', [Validators.pattern(`^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)`), Validators.required]],
+      creditCardNumber: ['', [Validators.pattern('[0-9]{16}'), Validators.required]],
+      verificationNumber: ['', [Validators.pattern('[0-9]{3}'), Validators.required]],
+      expirationMonth: ['', Validators.required],
+      expirationYear: ['', Validators.required]
     });
   }
 }
